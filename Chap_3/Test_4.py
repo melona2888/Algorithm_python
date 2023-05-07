67339a4c2c0949b6e48d2552c05ae55e46e81a30
melona2888 <melona2888@gmail.com>
2023-05-07 19:53:10 +0900

    Testfied

---
+++ Chap_3/Test_4.py
@@ -0,0 +1,17 @@
+# init value
+n, k = map(int, input().split())
+res = 0
+
+while True:
+    if n < k:
+        break
+    remain = n % k
+    n -= remain
+    res += remain
+
+    quot = n // k
+    n = quot
+    res += 1
+
+res += (n-1)
+print(res)
